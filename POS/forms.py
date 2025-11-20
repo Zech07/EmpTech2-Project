c00613fc171dd69645bfa447b294b6a88b8062e7
Chistian <mcbcastro@tip.edu.ph>
2025-11-21 07:49:42 +0800

    Fixed database

--- POS/forms.py
+++ POS/forms.py
@@ -3,7 +3,6 @@ from .models import Delivery
 
 class Change_Status(forms.ModelForm):
     class Meta:
-<<<<<<< HEAD
         model = models.Order
         fields = ['jug_status']
 
@@ -11,8 +10,3 @@ class Update_Customer(froms.ModelForm):
     class Meta:
         model = models.Order
         fields = ['first_name', 'last_name', 'phone_number', 'jug_tag']
-=======
-        model = Delivery
-        # Only include fields that actually exist in Delivery
-        fields = ['order', 'status']  # 'order' links to the customer/order
->>>>>>> b774a21d51044f5960c809fe83a88f852e9bfce8
