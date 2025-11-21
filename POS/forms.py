b8d43a4e2ec6fee3df25886592aeb94596cd80ff
Chistian <mcbcastro@tip.edu.ph>
2025-11-21 08:03:35 +0800

    cleaned files

--- POS/forms.py
+++ POS/forms.py
@@ -1,12 +1,52 @@
 from django import forms
-from .models import Delivery
+from .models import Order, Customer
+from django.contrib.auth.models import User
 
-class Change_Status(forms.ModelForm):
+class ChangeStatusForm(forms.ModelForm):
     class Meta:
-        model = models.Order
+        model = Order
         fields = ['jug_status']
+        widgets = {
+            'jug_status': forms.Select(attrs={'class': 'form-control'})
+        }
 
-class Update_Customer(froms.ModelForm):
+class UpdateCustomerForm(forms.ModelForm):
+    # User fields
+    first_name = forms.CharField(max_length=150, required=True)
+    last_name = forms.CharField(max_length=150, required=True)
+    email = forms.EmailField(required=True)
+    
     class Meta:
-        model = models.Order
-        fields = ['first_name', 'last_name', 'phone_number', 'jug_tag']
+        model = Customer
+        fields = ['phone', 'address', 'jug_tag']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        # Pre-populate User fields if instance exists
+        if self.instance and self.instance.user:
+            self.fields['first_name'].initial = self.instance.user.first_name
+            self.fields['last_name'].initial = self.instance.user.last_name
+            self.fields['email'].initial = self.instance.user.email
+        
+        # Add CSS classes
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+    
+    def save(self, commit=True):
+        customer = super().save(commit=False)
+        
+        # Update User model data
+        if customer.user:
+            customer.user.first_name = self.cleaned_data['first_name']
+            customer.user.last_name = self.cleaned_data['last_name']
+            customer.user.email = self.cleaned_data['email']
+            if commit:
+                customer.user.save()
+        
+        if commit:
+            customer.save()
+        
+        return customer
--- POS/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r'ws/pos/$', consumers.NotificationConsumer.as_asgi()),
-]
